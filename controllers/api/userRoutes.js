8ef1b0303ad305af6093b4848047c419c67d21c3
Sal Gamez <gamezsalvador@gmail.com>
2020-12-28 20:17:53 -0700

    Made changes some of the logic in userRoute.js to create/login and logout users, tested routes with insomnia

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,39 +1,52 @@
-const router = require('express').Router();
-const  { User } = require("../../models");
+const router = require("express").Router();
+const { User } = require("../../models");
 
 // Create new user
-router.post('/', async (req, res) => {
-    try {
-        const userData = await User.create(req.body);
-        req.session.user = {id: userData.id}
-        res.status(200).json(userData);
-    } catch (err) {
-        res.status(400).json(err);
-    }
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.user_id = userData.id;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Login
-router.post('/session', async (req, res) => {
-    const {email, password} = req.body
-    const user = await User.findOne({where: {email}})
-    if(user && await user.checkPassword(password)){
-        req.session.user = {id: user.id}
-        res.status(200).json(user); 
-        return 
+router.post("/session", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (user && (await user.checkPassword(password))) {
+      req.session.save(() => {
+        req.session.logged_in = true;
+        req.session.user_id = user.id;
+        res.status(200).json(user);
+      });
+      return;
     }
     res.status(401).send("Invalid login credentials!");
-})
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // Logout
-router.delete('/session', async (req, res) => {
-    if(req.session.user == null) {
-        res.status(401).send("Youre not logged in!");
-        return
+router.delete("/session", async (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status.end();
     }
-    req.session = null;
-    res.sendStatus(204);
-})
-
-
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
