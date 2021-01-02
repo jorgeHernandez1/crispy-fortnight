82aac39b0eebe5b1abda605282a65b908e0a6e63
Jorge <jhernan1337@gmail.com>
2021-01-02 14:15:10 -0700

    Added correct routes and links to buttons on front end. Added login.js and signup.js to front end which send request to backend

---
+++ public/js/signup.js
@@ -0,0 +1,26 @@
+const signupbtn = document.querySelector('#signupbtn');
+// Send request to API to sign user up
+signupbtn.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const displayName = document.querySelector('#display-name').value.trim();
+  const email = document.querySelector('#signup-email').value.trim();
+  const password = document.querySelector('#signup-pw').value.trim();
+
+  console.log(displayName,email,password)
+  const response = await fetch('/api/users/', {
+    method: 'POST',
+    body: JSON.stringify({
+      displayName,
+      email,
+      password,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    document.querySelector('#signup-error').textContent =
+      'Failed to sign up. Try again.';
+  }
+});
