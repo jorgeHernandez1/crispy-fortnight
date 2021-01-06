814ebeec6293ecc2e073c5fd7f44f9ab717e0bfd
Sal Gamez <gamezsalvador@gmail.com>
2021-01-05 19:59:31 -0700

    added new appoinment model and appointment.js file. Install TIMESTAMP npm package. Tested new table and columns show up in mysql

--- models/index.js
+++ models/index.js
@@ -1,3 +1,19 @@
 const User = require('./User');
+const Appointment = require('./Appointment');
 
-module.exports = { User };
+User.hasMany(Appointment,{
+    foreignKey: 'attendee_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Appointment,{
+    foreignKey: 'organizer_id',
+    onDelete: 'CASCADE'
+});
+
+Appointment.belongsTo(User, {
+    foreignKey: 'attendee_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = { User, Appointment};
